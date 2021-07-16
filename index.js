53bdf0b43c81b79f62720b744f4e5de68a85c01f
isaricciolini <87038843+isaricciolini@users.noreply.github.com>
2021-07-16 18:54:14 -0300

    páginas novas

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var textNomeVeterinario = document.getElementById('textNomeVeterinario');
 var textDataConsulta = document.getElementById('textDataConsulta');
 var textNomeCliente = document.getElementById('textNomeCliente');
 var codConsultaAlterar = 0;
+var modalAlterar = new bootstrap.Modal(document.getElementById('modalAlterar'), {});
 var textNomeAnimalAlterar = document.getElementById('textNomeAnimalAlterar');
 var textNomeVeterinarioAlterar = document.getElementById('textNomeVeterinarioAlterar');
 var textDataConsultaAlterar = document.getElementById('textDataConsultaAlterar');
 var textNomeClienteAlterar = document.getElementById('textNomeClienteAlterar');
+var textCodAnimal = document.getElementById('textCodAnimal');
+var textCodCliente = document.getElementById('textCodCliente');
+var textCodVeterinario = document.getElementById('textCodVeterinario');
+var textPeso = document.getElementById('textPeso');
+var textDescricao = document.getElementById('textDescricao');
 
 pesquisarConsultas();
 
@@ -26,8 +32,8 @@ function pesquisarConsultas() {
                 linha += `<td id="nomeAnimal${info.codConsulta}">${info.nomeAnimal}</td>`;
                 linha += `<td id="nomeCliente${info.codConsulta}">${info.nomeCliente}</td>`;
                 linha += `<td id="nomeVeterinario${info.codConsulta}">${info.nomeVeterinario}</td>`;
-                linha += `<td><a href="#" onclick="abrirAlterarConsulta(${info.codConsulta})">Alterar</a></td>`;
-                linha += `<td><a href="#" onclick="excluirConsulta(${info.codConsulta})">Excluir</a></td>`;
+                linha += `<td><a onclick="abrirAlterarConsulta(${info.codConsulta})">Alterar</a></td>`;
+                linha += `<td><a onclick="excluirConsulta(${info.codConsulta})">Excluir</a></td>`;
                 linha += '</tr>';
                 corpoTabela.innerHTML += linha;
             }
@@ -38,6 +44,38 @@ function pesquisarConsultas() {
     };
     xhttp.open('GET', `${url}/agenda`, true);
     xhttp.send();
+}
+
+function cadastrarConsulta() {
+    var dataConsulta = textDataConsulta.value;
+    var nomeAnimal = textNomeAnimal.value;
+    var nomeCliente = textNomeCliente.value;
+    var nomeVeterinario = textNomeVeterinario.value;
+    if (!dataConsulta || !nomeAnimal || !nomeCliente || !nomeAnimal || nomeVeterinario) {
+        alert('Preencha todos os dados para cadastrar!');
+        return;
+    }
+    var consulta = {
+        dataConsulta: dataConsulta,
+        codAnimal: codAnimal,
+        codCliente: codCliente,
+        codVeterinario: codVeterinario,
+        peso: 0,
+        descricao: ''
+    };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 201) {
+            alert(`Consulta de ${consulta.nomeAnimal} cadastrada com sucesso!`);
+            limpar();
+            pesquisarConsultas();
+        } else if (this.readyState == 4) {
+            alert('Não foi possível cadastrar a consulta.');
+        }
+    };
+    xhttp.open('POST', url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(consulta));    
 }
 
 function excluirConsulta(codConsulta) {
@@ -67,3 +105,41 @@ function abrirAlterarConsulta(codConsulta) {
     modalAlterar.show();
 }
 
+function alterarConsulta() {
+    var dataConsulta = textDataConsultaAlterar.value;
+    var nomeAnimal = textNomeAnimalAlterar.value;
+    var nomeCliente = textNomeClienteAlterar.value;
+    var nomeVeterinario = textNomeVeterinarioAlterar.value;
+    if (!dataConsulta || !nomeAnimal || !nomeCliente || !nomeVeterinario) {
+        alert('Preencha todos os dados para alterar!');
+        return;
+    }
+    var consulta = {
+        dataConsulta: dataConsulta,
+        nomeAnimal: nomeAnimal,
+        nomeCliente: nomeCliente,
+        nomeVeterinario: nomeVeterinario,
+        codConsulta: codConsultaAlterar
+    };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(`Consulta de ${consulta.nomeAnimal} alterado(a) com sucesso!`);
+            limpar();
+            pesquisarConsultas();
+            modalAlterar.hide();
+        } else if (this.readyState == 4) {
+            alert('Não foi possível alterar a consulta.');
+        }
+    };
+    xhttp.open('PUT', url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(consulta)); 
+}
+
+function limpar() {
+    textDataConsulta.value = '';
+    textNomeAnimal.value = '';
+    textNomeCliente.value = '';
+    textNomeVeterinario.value = '';
+}
