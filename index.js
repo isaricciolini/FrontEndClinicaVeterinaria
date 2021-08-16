23a5e01df735109a602e8e45056629ba6f6ea6bf
LuisBigHouse <70987212+LuisBigHouse@users.noreply.github.com>
2021-08-16 13:12:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ function logar() {
 }
 
 function pesquisarCRMV() {
-        corpoTabela.innerHTML = '';
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
